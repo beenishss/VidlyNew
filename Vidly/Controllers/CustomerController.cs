9dd0377b3eec1ad7794314eaf955e0c858e59baf
Beenish Shaikh <beenish.sshaikh@gmail.com>
2021-01-16 22:13:11 -0500

    after migration

---
+++ Vidly/Controllers/CustomerController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            List<Customer> cust = GetCustomers();
+            return View(cust);
+        }
+        public ActionResult Details(int id)
+        {
+          
+           var customer= GetCustomers().Where(i=>i.Id==id).FirstOrDefault();
+            return View(customer);
+        }
+        public List<Customer> GetCustomers()
+        {
+            List<Customer> customer = new List<Customer>()
+            {
+                new Customer{Id=1,Name="Beenish"},
+                new Customer{Id=2,Name="Momina"}
+            };
+            return customer;
+        }
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModel;
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Random()
+        {
+
+            var movie = new Movie() {Id=1, Name = "Sherk!" };
+            var customer = GetCustomers();
+            var viewmodel = new RandomMovieViewModel(); 
+            viewmodel.customer = customer;
+            viewmodel.movie = movie;
+            return View(viewmodel);
+        }
+        public List<Customer> GetCustomers()
+        {
+            var customer = new List<Customer>
+            { new Customer{Id=1,Name="Beenish"},
+              new Customer{Id=2,Name="Mubeen"} };
+
+              return customer;
+        }
+        
+    }
+}
